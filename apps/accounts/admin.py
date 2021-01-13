052cb2aa4eee3326898c7b46b4cd62fe4dcd4b9b
komeilyahyazade <komeilyahyazade@gmail.com>
2021-01-14 00:24:49 +0330

    admin fields added

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
+
+from .models import User
+
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'birth_date', 'province')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    birth_date = models.CharField(
+        max_length=128
+    )
+    province = models.CharField(
+        max_length=128
+    )
