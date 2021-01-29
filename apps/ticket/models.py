c4f9e0a96e724db5e65e17b28331d18f3a8119c1
sreyhani <sajjad7997@gmail.com>
2021-01-29 17:50:18 +0330

    ticket model & serializer created

---
+++ apps/ticket/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Ticket(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='ticket')
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField()
+    title = models.CharField(max_length=50)
+    text = models.TextField(max_length=10000)
+
+    def __str__(self):
+        return '%s %s' % (self.title, self.author.username)
+
+
+class Reply(models.Model):
+    ticket = models.ForeignKey(
+        Ticket, on_delete=models.CASCADE, related_name='replies')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='ticket_replies')
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s' % (self.user)
+
---
+++ apps/ticket/serializer.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.conf import settings
+
+from apps.ticket.models import *
+
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reply
+        fields = ['user', 'text', 'date']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.request.user
+        return Reply.objects.create(**validated_data)
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    replies = ReplySerializer(many=True)
+    num_replies = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Ticket
+        fields = ['replies', 'num_replies', 'date', 'image',
+                  'title', 'author', 'text']
+
+    def get_num_replies(self, obj):
+        return obj.replies.count()
