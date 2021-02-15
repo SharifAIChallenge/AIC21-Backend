be81a22818a6a3247c31eb412fc596778cf7cf89
alireza <alirezashateri7@gmail.com>
2021-02-15 18:35:15 +0330

    Add ticket api

--- apps/ticket/views.py
+++ apps/ticket/views.py
@@ -8,7 +8,6 @@ from .serializers import TicketSerializer
 
 class TicketAPIView(GenericAPIView):
     serializer_class = TicketSerializer
-    lookup_field = 'pk'
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
@@ -20,5 +19,11 @@ class TicketAPIView(GenericAPIView):
             status=status.HTTP_201_CREATED
         )
 
-    def get(self, request, token_uuid):
-        pass
+    def get(self, request, token_id):
+        ticket = get_object_or_404(Ticket, id=token_id)
+        data = self.get_serializer(instance=ticket).data
+
+        return Response(
+            data={'data': data},
+            status=status.HTTP_200_OK
+        )
