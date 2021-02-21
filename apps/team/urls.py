cad7ffae7cf1e6335e08f8e85c6322a71b7c7323
arman <armanzarei1378@gmail.com>
2021-02-21 20:29:19 +0330

    Update Team API

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from apps.team.views import TeamAPIView, TeamLeaveAPIView, TeamInfoAPIView
+from apps.team.views import TeamAPIView
 
 app_name = 'team'
 
 urlpatterns = [
-    path('', view=TeamAPIView.as_view(), name="create_team"),
-    path("leave", view=TeamLeaveAPIView.as_view(), name="leave_team"),
-    path('<str:team_id>', view=TeamInfoAPIView.as_view(), name="get_team"),
+    path('', view=TeamAPIView.as_view(), name="team_operations"),
+    # path('<str:team_id>', view=TeamAPIView.as_view(), name="get_team"),
 ]
--- apps/team/views.py
+++ apps/team/views.py
@@ -7,8 +7,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
+from .exceptions import NoTeamException
 from .models import Team
-from .serializers import TeamSerializer, TeamInfoSerializer
+from .permissions import HasTeam, NoTeam
+from .serializers import TeamSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 
 
@@ -16,6 +18,7 @@ class TeamAPIView(GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = TeamSerializer
     parser_classes = [MultiPartParser, FormParser]
+    queryset = Team.objects.all()
 
     def post(self, request):
         team = self.get_serializer(data=request.data)
@@ -24,30 +27,41 @@ class TeamAPIView(GenericAPIView):
 
         return Response(
             data={
-                "message": "Team created successfully"
+                "data": team.data
             },
             status=status.HTTP_200_OK
         )
 
-class TeamLeaveAPIView(GenericAPIView):
-    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        team = request.user.team
+        data = self.get_serializer(team).data
 
-    def post(self, request):
+        return Response(data)
+
+    def put(self, request):
+        team = self.get_serializer(data=request.data, instance=request.user.team, partial=True)
+        team.is_valid(raise_exception=True)
+        team.save()
+
+        return Response(
+            data={
+                "data": team.data
+            },
+            status=status.HTTP_200_OK
+        )
+
+    def delete(self, request):
         current_user = request.user
         current_user.team = None
         current_user.save()
 
         return Response(data={"message": "You left the team"}, status=status.HTTP_200_OK)
 
+    def get_permissions(self):
+        new_permissions = self.permission_classes
+        if self.request.method in ['PUT', 'GET']:
+            new_permissions += [HasTeam]
+        if self.request.method == 'POST':
+            new_permissions += [NoTeam]
 
-class TeamInfoAPIView(GenericAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = TeamInfoSerializer
-    queryset = Team.objects.all()
-
-    def get(self, req, team_id):
-        try:
-            data = self.get_serializer(self.get_queryset().get(pk=team_id)).data
-            return Response(data)
-        except Team.DoesNotExist:
-            raise Http404
+        return [permission() for permission in new_permissions]
