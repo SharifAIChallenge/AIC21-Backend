92b876bca530cb614f9d3d7372b0b8742f1add30
Amin <m.agasemzadeh@gmail.com>
2021-02-24 10:36:08 +0330

    just a url

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -25,14 +25,14 @@ __all__ = ('LoginAPIView', 'SignUpAPIView', 'ActivateAPIView', 'LogoutAPIView',
 
 CLIENT_ID = '864043474548-9is9rd8jbf3bbq4tdrhsfdjnivasj7l6.apps.googleusercontent.com'
 
-
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from django.conf import settings
 
+
 class GoogleLogin(SocialLoginView):
-    authentication_classes = [] # disable authentication
+    authentication_classes = []  # disable authentication
     adapter_class = GoogleOAuth2Adapter
     callback_url = "http://localhost:3000"
     client_class = OAuth2Client
