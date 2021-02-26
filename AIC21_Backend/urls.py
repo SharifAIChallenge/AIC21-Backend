47364af1391e68425501ff82d0d6a282cc76ce16
alireza <alirezashateri7@gmail.com>
2021-02-26 20:03:35 +0330

    Fix urlpatterns

--- AIC21_Backend/urls.py
+++ AIC21_Backend/urls.py
@@ -46,6 +46,6 @@ urlpatterns = [
     path('api/courses/', include('apps.course.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
