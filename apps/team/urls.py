a9f5aa0467200045641df8ab3ae19562abb23c7c
alireza <alirezashateri7@gmail.com>
2021-03-08 19:55:56 +0330

    Update teams urls.py

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -1,27 +1,38 @@
 from django.urls import path
 
-from apps.team.views import TeamAPIView, TeamSearchAPIView, TeamInfoAPIView, IncompleteTeamInfoListAPIView, \
-    UserReceivedPendingInvitationListAPIView,UserReceivedResolvedInvitationListAPIView\
+from apps.team.views import TeamAPIView, TeamSearchAPIView, TeamInfoAPIView, \
+    IncompleteTeamInfoListAPIView, \
+    UserReceivedPendingInvitationListAPIView, \
+    UserReceivedResolvedInvitationListAPIView \
     , TeamSentInvitationListAPIView, UserAnswerInvitationAPIView, \
-    UserSentInvitationListAPIView, TeamPendingInvitationListAPIView, TeamAnswerInvitationAPIView
+    UserSentInvitationListAPIView, TeamPendingInvitationListAPIView, \
+    TeamAnswerInvitationAPIView
 
 app_name = 'team'
 
 urlpatterns = [
     path('', view=TeamAPIView.as_view(), name="team_operations"),
     path('search', view=TeamSearchAPIView.as_view(), name="team_search"),
-    path('incomplete', view=IncompleteTeamInfoListAPIView.as_view(), name="get_incomplete_teams"),
-    path('invitations/user_pending', view=UserReceivedPendingInvitationListAPIView.as_view(),
+    path('incomplete', view=IncompleteTeamInfoListAPIView.as_view(),
+         name="get_incomplete_teams"),
+    path('invitations/user_pending',
+         view=UserReceivedPendingInvitationListAPIView.as_view(),
          name="get_user_received_pending_invitations"),
-    path('invitations/user_received_resolved',view=UserReceivedResolvedInvitationListAPIView.as_view(),
+    path('invitations/history',
+         view=UserReceivedResolvedInvitationListAPIView.as_view(),
          name="get_user_received_resolved_invitations"),
-    path('invitations/team_pending', view=TeamPendingInvitationListAPIView.as_view(),
+    path('invitations/team_pending',
+         view=TeamPendingInvitationListAPIView.as_view(),
          name="get_team_pending_invitations"),
-    path('invitations/team_pending/<str:invitation_id>', view=TeamAnswerInvitationAPIView.as_view(),
+    path('invitations/team_pending/<str:invitation_id>',
+         view=TeamAnswerInvitationAPIView.as_view(),
          name="team_answer_invitation"),
-    path('invitations/user_pending/<str:invitation_id>', view=UserAnswerInvitationAPIView.as_view(),
+    path('invitations/user_pending/<str:invitation_id>',
+         view=UserAnswerInvitationAPIView.as_view(),
          name="user_answer_invitation"),
-    path('invitations/user_sent', view=UserSentInvitationListAPIView.as_view(), name="user_sent_invitation_list"),
-    path('invitations/team_sent', view=TeamSentInvitationListAPIView.as_view(), name="team_sent_invitation_list"),
+    path('invitations/user_sent', view=UserSentInvitationListAPIView.as_view(),
+         name="user_sent_invitation_list"),
+    path('invitations/team_sent', view=TeamSentInvitationListAPIView.as_view(),
+         name="team_sent_invitation_list"),
     path('<str:team_id>', view=TeamInfoAPIView.as_view(), name="get_team"),
 ]
