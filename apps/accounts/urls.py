cf1955b583aa627c4ed7b93283a49b675375e7e5
alireza <alirezashateri7@gmail.com>
2021-03-16 11:55:56 +0330

    Fix accounts urls bug

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('login', view=LoginAPIView.as_view(), name='login'),
     path('social-login', view=GoogleLoginAPIView.as_view(),
          name='social_login'),
-    path('is-active', view=IsActivatedAPIView.as_view(), name='is_active')
+    path('is-active', view=IsActivatedAPIView.as_view(), name='is_active'),
     path('signup', view=SignUpAPIView.as_view(), name='signup'),
     path('activate/<slug:eid>/<slug:token>', view=ActivateAPIView.as_view(),
          name='activate'),
