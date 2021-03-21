934b91440277946b2052aafd1ed0d1b598ce9439
alireza <alirezashateri7@gmail.com>
2021-03-21 23:34:08 +0330

    Update gamedoc model

--- apps/gamedoc/serializers.py
+++ apps/gamedoc/serializers.py
@@ -5,4 +5,4 @@ from apps.gamedoc.models import Gamedoc
 class GamedocSerializer(serializers.ModelSerializer):
     class Meta:
         model = Gamedoc
-        fields = ['link', 'title']
+        fields = ['link', 'title', 'repo_name', 'user_name']
