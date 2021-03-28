5bfb6dbd211c55145ebe9e3c3c046858be5a56f2
alireza <alirezashateri7@gmail.com>
2021-03-29 01:28:33 +0430

    Update ScoreboardRowSerializer

--- apps/challenge/serializers/scoreboard_row.py
+++ apps/challenge/serializers/scoreboard_row.py
@@ -1,14 +1,11 @@
 from rest_framework import serializers
 
 from apps.challenge.models import ScoreboardRow
+from apps.team.serializers import TeamInfoSerializer
 
 
 class ScoreboardRowSerializer(serializers.ModelSerializer):
-    team_name = serializers.SerializerMethodField('_team_name')
-
-    @staticmethod
-    def _team_name(obj: ScoreboardRow):
-        return obj.team.name
+    team = TeamInfoSerializer(read_only=True)
 
     class Meta:
         model = ScoreboardRow
