d88b836128282efa0feb942a3f65ad2f90758f3c
alireza <alirezashateri7@gmail.com>
2021-03-29 00:20:52 +0430

    Add source_team_name and target_teaem_name to RequestSerializer

--- apps/challenge/serializers/request.py
+++ apps/challenge/serializers/request.py
@@ -4,10 +4,23 @@ from apps.challenge.models import Request
 
 
 class RequestSerializer(serializers.ModelSerializer):
+    source_team_name = serializers.SerializerMethodField('_source_team_name')
+    target_team_name = serializers.SerializerMethodField('_target_team_name')
+
+    @staticmethod
+    def _source_team_name(obj: Request):
+        return obj.source_team.name
+
+    @staticmethod
+    def _target_team_name(obj: Request):
+        return obj.target_team.name
+
     class Meta:
         model = Request
-        fields = ('id', 'source_team', 'target_team', 'status', 'type')
-        read_only_fields = ('source_team', 'status')
+        fields = ('id', 'source_team', 'target_team', 'status', 'type',
+                  'source_team_name', 'target_team_name')
+        read_only_fields = ('source_team', 'status', 'source_team_name',
+                            'target_team_name')
 
     def create(self, validated_data):
         validated_data['source_team'] = self.context['request'].user.team
